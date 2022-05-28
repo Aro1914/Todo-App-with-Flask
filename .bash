dcbd608ec67427601b3d28eb21b1aaaedad096be
Emmanuel Agbavwe <agbavweisaac@gmail.com>
2022-05-28 10:42:28 +0100

    Updated .bash

--- .bash
+++ .bash
@@ -1,13 +1,4 @@
-python -m venv venv
-
-if [ -d venv/Scripts ]; then
-    . venv/Scripts/activate
-    venv/Scripts/python.exe -m pip install -r requirements.txt
-elif [ -d venv/bin ]; then
-    . venv/bin/activate
-    venv/bin/python -m pip install -r requirements.txt
-fi
-
+python -m pip install -r requirements.txt
 flask db init 
 flask db migrate
 flask db upgrade
